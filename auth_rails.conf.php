5c39314333c1fd9b63fe94a7a755eabcceefacfc
shutty <grebennikov.roman@gmail.com>
2012-09-27 14:06:11 +0400

    code cleanup

--- auth_rails.conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  global $rails_db_user, $rails_db_password, $rails_db_host, $rails_db_name;
-  $rails_db_user = 'root';
-  $rails_db_password = '';
-  $rails_db_host = 'localhost';
-  $rails_db_name = 'bookworm';
-
-?>
