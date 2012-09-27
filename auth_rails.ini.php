1ac3e725a6dacb9b02ddc5c008e8cd0d3533c216
shutty <grebennikov.roman@gmail.com>
2012-09-27 15:17:43 +0400

    add placeholders for db creds

--- auth_rails.ini.php
+++ auth_rails.ini.php
@@ -2,8 +2,8 @@
 ;/*
 
 db_host = localhost
-db_user = root
-db_password = 
-db_name = bookworm
+db_user = your_database_username
+db_password = your_database_password
+db_name = your_database_name
 
 ;*/ ?>
